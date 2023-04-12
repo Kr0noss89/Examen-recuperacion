27bfa253527c95f6ab0c57fee99cfc635cb0b66a
Kr0noss89 <esswarrh@gmail.com>
2023-04-12 10:16:43 +0200

    Guardado

---
+++ Assets/Scripts/GroundSensor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+    private PlayerController controller;
+    public bool isGrounded;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 3)
+        {
+            isGrounded = true;
+            controller.anim.SetBool("IsJumping", false);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 3)
+        {
+            isGrounded = true;
+            controller.anim.SetBool("IsJumping", false);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 3)
+        {
+            isGrounded = false;
+            controller.anim.SetBool("IsJumping", true);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public float playerSpeed = 5.5f;
+    public float jumpForce = 3f;
+    private Rigidbody2D rBody;
+    private GroundSensor sensor;
+    public Animator anim;
+
+    
+    float horizontal;
+
+    
+    void Start()
+    {
+       
+        
+        rBody = GetComponent<Rigidbody2D>();
+        sensor = GameObject.Find("GroundSensor").GetComponent<GroundSensor>();
+        anim = GetComponent<Animator>();
+        
+    }
+
+    
+    void Update()
+
+        {
+            horizontal = Input.GetAxis("Horizontal"); 
+
+            if(horizontal < 0)
+            {
+                
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                anim.SetBool("IsRunning", true);
+            }
+            else if(horizontal > 0)
+            {
+                
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                anim.SetBool("IsRunning", true);
+            }
+            else
+            {
+                anim.SetBool("IsRunning", false);
+            }
+
+            if(Input.GetButtonDown("Jump") && sensor.isGrounded)
+            {
+                rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                anim.SetBool("IsJumpinng", true);
+            }
+
+        }    
+        
+    
+
+    void FixedUpdate()
+    {
+        rBody.velocity = new Vector2(horizontal * playerSpeed, rBody.velocity.y);
+    }
+    
+}
